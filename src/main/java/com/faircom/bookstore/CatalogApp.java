78873823fade6855148970c5de43864325e624cb
Shane K Johnson <shane.johnson@faircom.com>
2021-10-05 08:36:35 -0700

    Update license and RIO jar

---
+++ src/main/java/com/faircom/bookstore/CatalogApp.java
@@ -0,0 +1,8 @@
+package com.faircom.bookstore;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/catalog")
+public class CatalogApp extends Application {
+}
